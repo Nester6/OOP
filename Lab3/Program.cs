127912f5433b3df6ea7fa61800bb38ba5a0298f1
Nester6 <159240715+Nester6@users.noreply.github.com>
2025-04-21 10:17:44 +0300

    Add files via upload

---
+++ Lab3/Program.cs
@@ -0,0 +1,120 @@
+﻿using System;
+
+abstract class Pair
+{
+    public abstract Pair Add(Pair other);
+    public abstract Pair Subtract(Pair other);
+    public abstract Pair Multiply(Pair other);
+    public abstract Pair Divide(Pair other);
+    public abstract void Print();
+}
+
+class Fraction : Pair
+{
+    public int WholePart { get; set; } // Ціла частина (може бути від'ємною)
+    public uint FractionalPart { get; set; } // Дробова частина (тільки додатне число)
+
+    public Fraction(int whole, uint fractional)
+    {
+        WholePart = whole;
+        FractionalPart = fractional;
+    }
+
+    public override Pair Add(Pair other)
+    {
+        if (other is Fraction f)
+            return new Fraction(this.WholePart + f.WholePart, this.FractionalPart + f.FractionalPart);
+        throw new ArgumentException("Invalid type for operation");
+    }
+
+    public override Pair Subtract(Pair other)
+    {
+        if (other is Fraction f)
+            return new Fraction(this.WholePart - f.WholePart, this.FractionalPart - f.FractionalPart);
+        throw new ArgumentException("Invalid type for operation");
+    }
+
+    public override Pair Multiply(Pair other)
+    {
+        if (other is Fraction f)
+            return new Fraction(this.WholePart * f.WholePart, this.FractionalPart * f.FractionalPart);
+        throw new ArgumentException("Invalid type for operation");
+    }
+
+    public override Pair Divide(Pair other)
+    {
+        if (other is Fraction f && f.WholePart != 0 && f.FractionalPart != 0)
+            return new Fraction(this.WholePart / f.WholePart, this.FractionalPart / f.FractionalPart);
+        throw new ArgumentException("Cannot divide by zero or invalid type");
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine($"{WholePart}.{FractionalPart}");
+    }
+}
+
+class FuzzyNumber : Pair
+{
+    public double X { get; set; }  // Центр нечіткого числа
+    public double L { get; set; }  // Нижня межа
+    public double R { get; set; }  // Верхня межа
+
+    public FuzzyNumber(double x, double l, double r)
+    {
+        X = x;
+        L = l;
+        R = r;
+    }
+
+    public override Pair Add(Pair other)
+    {
+        if (other is FuzzyNumber f)
+            return new FuzzyNumber(this.X + f.X, this.L + f.L, this.R + f.R);
+        throw new ArgumentException("Invalid type for operation");
+    }
+
+    public override Pair Subtract(Pair other)
+    {
+        if (other is FuzzyNumber f)
+            return new FuzzyNumber(this.X - f.X, this.L - f.L, this.R - f.R);
+        throw new ArgumentException("Invalid type for operation");
+    }
+
+    public override Pair Multiply(Pair other)
+    {
+        if (other is FuzzyNumber f)
+            return new FuzzyNumber(this.X * f.X, this.L * f.L, this.R * f.R);
+        throw new ArgumentException("Invalid type for operation");
+    }
+
+    public override Pair Divide(Pair other)
+    {
+        if (other is FuzzyNumber f && f.X != 0 && f.L != 0 && f.R != 0)
+            return new FuzzyNumber(this.X / f.X, this.L / f.L, this.R / f.R);
+        throw new ArgumentException("Cannot divide by zero or invalid type");
+    }
+
+    public override void Print()
+    {
+        Console.WriteLine($"({X}, {L}, {R})");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Тестування Fraction
+        Fraction frac1 = new Fraction(2, 50);
+        Fraction frac2 = new Fraction(1, 25);
+        Pair sumFrac = frac1.Add(frac2);
+        sumFrac.Print();
+
+        // Тестування FuzzyNumber
+        FuzzyNumber fuzz1 = new FuzzyNumber(3.5, 1.2, 2.1);
+        FuzzyNumber fuzz2 = new FuzzyNumber(2.0, 0.8, 1.5);
+        Pair sumFuzz = fuzz1.Add(fuzz2);
+        sumFuzz.Print();
+    }
+}
---
+++ Lab4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        // 1. Одновимірний масив
+        double[] array = { 0, -5.3, 3.1, 0, 2.7, -1.5, 4.8, 0 };
+
+        // а) Мінімальний елемент масиву
+        double minElement = array.Min();
+        Console.WriteLine($"Мінімальний елемент: {minElement}");
+
+        // б) Сума елементів між першим і другим додатними числами
+        int firstPos = Array.FindIndex(array, x => x > 0);
+        int secondPos = Array.FindIndex(array, firstPos + 1, x => x > 0);
+        double sumBetween = (firstPos != -1 && secondPos != -1)
+            ? array.Skip(firstPos + 1).Take(secondPos - firstPos - 1).Sum()
+            : 0;
+        Console.WriteLine($"Сума між першим і другим додатними числами: {sumBetween}");
+
+        // Перетворення: спочатку нулі, потім решта
+        double[] transformedArray = array.OrderBy(x => x != 0).ToArray();
+        Console.WriteLine("Перетворений масив: " + string.Join(", ", transformedArray));
+
+        // 2. Двовимірний масив
+        int[,] matrix = {
+            { 1,  4,  7 },
+            { -2, 5,  8 },
+            { -9, 3,  6 }
+        };
+
+        Console.WriteLine("Двовимірний масив:");
+        PrintMatrix(matrix);
+
+        // а) Порівняння нижніх кутів
+        int lowerLeft = matrix[matrix.GetLength(0) - 1, 0]; // Лівий нижній кут
+        int lowerRight = matrix[matrix.GetLength(0) - 1, matrix.GetLength(1) - 1]; // Правий нижній кут
+        Console.WriteLine($"Менший з нижніх кутів: {Math.Min(lowerLeft, lowerRight)}");
+
+        // б) Порівняння верхнього правого і нижнього лівого кута
+        int upperRight = matrix[0, matrix.GetLength(1) - 1]; // Верхній правий кут
+        Console.WriteLine($"Менший з верхнього правого і нижнього лівого кута: {Math.Min(upperRight, lowerLeft)}");
+    }
+
+    // Функція для виводу матриці
+    static void PrintMatrix(int[,] matrix)
+    {
+        for (int i = 0; i < matrix.GetLength(0); i++)
+        {
+            for (int j = 0; j < matrix.GetLength(1); j++)
+            {
+                Console.Write(matrix[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+    }
+}
